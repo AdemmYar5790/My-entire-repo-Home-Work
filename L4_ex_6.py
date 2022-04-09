4f3465d3aa94c704feb4fcdac35cd12d6c8dc0f5
AdemmYar5790 <101930367+AdemmYar5790@users.noreply.github.com>
2022-04-09 23:07:01 +0300

    Delete L4_ex_6.py

--- L4_ex_6.py
+++
@@ -1,8 +0,0 @@
-def gen1():
-    g = int(input('Введите стартовое число: '))
-    from itertools import islice
-    from itertools import count
-
-    for i in islice(count(g), 6):
-        print(i)
-gen1()
