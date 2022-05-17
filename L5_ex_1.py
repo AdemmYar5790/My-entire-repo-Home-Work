c79d2c4f9672904b13f4119e70cc799cc5c5c756
AdemmYar5790 <101930367+AdemmYar5790@users.noreply.github.com>
2022-05-17 15:13:10 +0300

    Delete L5_ex_1.py

--- L5_ex_1.py
+++
@@ -1,4 +0,0 @@
-print('Enter data for writing in file: \nFor ending of writing enter empty string:')
-with open('ex_1.txt', 'w', encoding='utf-8') as my_hw5:
-    while (line := input()) != '':
-        my_hw5.write(f"{line}\n")
