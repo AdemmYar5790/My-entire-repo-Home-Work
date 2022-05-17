e91fa4f990f3faa2e1fb77dc04f6feae87e12a1d
AdemmYar5790 <101930367+AdemmYar5790@users.noreply.github.com>
2022-05-17 15:15:31 +0300

    Delete ex3.py

--- ex3.py
+++
@@ -1,9 +0,0 @@
-def my_func(arg1, arg2, arg3):
-    print(f'The Sum. of the two largest arguments is: {arg1 + arg2 + arg3 - min([arg1, arg2, arg3])}')
-
-my_func(
-    int(input('Arg. 1: ')),
-    int(input('Arg. 2: ')),
-    int(input('Arg. 3: ')),
-)
-
